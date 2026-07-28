730a02d22910fee8a9cc6ee92682a5560d6738a9
Manojhajam <manojhajam4@gmail.com>
2026-07-28 14:38:11 +0545

    Update NoticeBar component styling for improved responsiveness and consistency

--- src/components/notice/NoticeBar.tsx
+++ src/components/notice/NoticeBar.tsx
@@ -1,20 +1,17 @@
 const NoticeBar = () => {
   return (
     <div className="container overflow-hidden py-2 mt-2">
-      <div className="animate-marquee flex gap-4 w-max whitespace-nowrap text-white text-2xl">
-        <span className="px-4 bg-green-800">
+      <div className="animate-marquee flex gap-4 w-max whitespace-nowrap text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+        <span className="px-2 sm:px-4 bg-green-800">
           🚀 Mahaagromart is launching on 01 January 2026 - stay tuned
         </span>
-        {/* <span className="px-4 bg-green-800">•</span> */}
-        <span className="px-4 bg-green-800">
+        <span className="px-2 sm:px-4 bg-green-800">
           Bigger • Faster • Smarter — Mahaagromart V2 launches
         </span>
-        {/* <span className="px-4">•</span> */}
-        <span className="px-4 bg-green-800">
+        <span className="px-2 sm:px-4 bg-green-800">
           🚀 Mahaagromart is launching on 01 January 2026 - stay tuned
         </span>
-        {/* <span className="px-4">•</span> */}
-        <span className="px-4 bg-green-800">
+        <span className="px-2 sm:px-4 bg-green-800">
           Bigger • Faster • Smarter — Mahaagromart V2 launches
         </span>
       </div>
